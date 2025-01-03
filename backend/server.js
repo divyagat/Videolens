cad49eadd4548b4e3573bca317914834c9ea0b5c
coderpushkargharate <pushkargharate7@gmail.com>
2025-01-03 15:53:47 +0530

    sadfa

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ require("dotenv").config(); // Load environment variables
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
